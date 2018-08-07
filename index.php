97f9989f5f53f6d91e365f4416492cc7a7567868
Emerson Garcia <emersomggarcia86@gmail.com>
2018-08-07 14:16:19 -0300

    Segundo commit do projeto

--- index.php
+++ index.php
@@ -106,12 +106,225 @@
        	  <!-- BUSCA COMUM -->
        	  <section id="busca-comum">
        	  	 <div id="busca-comum">
+
        	  	 	<form action="" method="POST" name="busca_comum" class="busca-comum">
-       	  	 		<input type="text" name="busca" class="busca-comum" placeholder="Busca comum:">
+       	  	 		<input type="text" name="s" class="busca-comum" placeholder="Busca comum:">
        	  	 		<input type="submit" value="Buscar" name="buscar" class="btn_buscacomum">
        	  	 	</form>
+
        	  	 </div>
        	  </section>
+
+       	  <!-- LISTA -->
+       	  <section id="lista">
+       	  	<article id="lista">
+
+                <!-- LISTA 1 -->
+       	  		<ul class="lista_um">
+       	  			<li>
+       	  				<figure class="lista-img">
+       	  					<img src="images/" alt="" class="lista-img">
+       	  					<br>
+       	  					<h2>Imóvel 1</h2>
+       	  					<br>
+       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+       	  					<br>
+       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+       	  				</figure>
+       	  			</li>
+       	  		</ul>
+
+       	  		<!-- LISTA 2 -->
+       	  		<ul class="lista_dois">
+       	  			<li>
+       	  				<figure class="lista-img">
+       	  					<img src="images/" alt="" class="lista-img">
+       	  					<br>
+       	  					<h2>Imóvel 1</h2>
+       	  					<br>
+       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+       	  					<br>
+       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+       	  				</figure>
+       	  			</li>
+       	  		</ul>
+
+       	  		<!-- LISTA 3 -->
+       	  		<ul class="lista_tres">
+       	  			<li>
+       	  				<figure class="lista-img">
+       	  					<img src="images/" alt="" class="lista-img">
+       	  					<br>
+       	  					<h2>Imóvel 1</h2>
+       	  					<br>
+       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+       	  					<br>
+       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+       	  				</figure>
+       	  			</li>
+       	  		</ul>
+
+       	  		<!-- LISTA 4 -->
+       	  		<ul class="lista_quatro">
+       	  			<li>
+       	  				<figure class="lista-img">
+       	  					<img src="images/" alt="" class="lista-img">
+       	  					<br>
+       	  					<h2>Imóvel 1</h2>
+       	  					<br>
+       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+       	  					<br>
+       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+       	  				</figure>
+       	  			</li>
+       	  		</ul>
+
+       	  		<!-- LISTA 5 -->
+       	  		<ul class="lista_cinco">
+       	  			<li>
+       	  				<figure class="lista-img">
+       	  					<img src="images/" alt="" class="lista-img">
+       	  					<br>
+       	  					<h2>Imóvel 1</h2>
+       	  					<br>
+       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+       	  					<br>
+       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+       	  				</figure>
+       	  			</li>
+       	  		</ul>
+
+       	  		<!-- LISTA 6 -->
+       	  		<ul class="lista_seis">
+       	  			<li>
+       	  				<figure class="lista-img">
+       	  					<img src="images/" alt="" class="lista-img">
+       	  					<br>
+       	  					<h2>Imóvel 1</h2>
+       	  					<br>
+       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+       	  					<br>
+       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+       	  				</figure>
+       	  			</li>
+       	  		</ul>
+
+       	  		<!-- LISTA 7 -->
+       	  		<ul class="lista_sete">
+       	  			<li>
+       	  				<figure class="lista-img">
+       	  					<img src="images/" alt="" class="lista-img">
+       	  					<br>
+       	  					<h2>Imóvel 1</h2>
+       	  					<br>
+       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+       	  					<br>
+       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+       	  				</figure>
+       	  			</li>
+       	  		</ul>
+
+       	  		<figure class="banner-anuncie">
+       	  			<a href="#"><img src="images/anuncie.jpg" alt="anuncie" class="anuncie"></a>
+       	  		</figure>
+
+                <!-- LISTA RELACIONADA -->
+       	  		<div id="lista-relacionada">
+
+       	  			<!-- LISTA RELACIONADA 1 -->
+	       	  		<ul class="lista_rel_1">
+	       	  			<li>
+	       	  				<figure class="lista-rel-img">
+	       	  					<img src="images/" alt="" class="lista-img">
+	       	  					<br>
+	       	  					<h2>Imóvel 1</h2>
+	       	  					<br>
+	       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+	       	  					<br>
+	       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+	       	  				</figure>
+	       	  			</li>
+	       	  		</ul>
+
+	       	  		<!-- LISTA RELACIONADA 2 -->
+	       	  		<ul class="lista_rel_2">
+	       	  			<li>
+	       	  				<figure class="lista-rel-img">
+	       	  					<img src="images/" alt="" class="lista-img">
+	       	  					<br>
+	       	  					<h2>Imóvel 1</h2>
+	       	  					<br>
+	       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+	       	  					<br>
+	       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+	       	  				</figure>
+	       	  			</li>
+	       	  		</ul>
+
+	       	  		<!-- LISTA RELACIONADA 3 -->
+	       	  		<ul class="lista_rel_3">
+	       	  			<li>
+	       	  				<figure class="lista-rel-img">
+	       	  					<img src="images/" alt="" class="lista-img">
+	       	  					<br>
+	       	  					<h2>Imóvel 1</h2>
+	       	  					<br>
+	       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+	       	  					<br>
+	       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+	       	  				</figure>
+	       	  			</li>
+	       	  		</ul>
+
+	       	  		<!-- LISTA RELACIONADA 4 -->
+	       	  		<ul class="lista_rel_4">
+	       	  			<li>
+	       	  				<figure class="lista-rel-img">
+	       	  					<img src="images/" alt="" class="lista-img">
+	       	  					<br>
+	       	  					<h2>Imóvel 1</h2>
+	       	  					<br>
+	       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+	       	  					<br>
+	       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+	       	  				</figure>
+	       	  			</li>
+	       	  		</ul>
+
+	       	  		<!-- LISTA RELACIONADA 5 -->
+	       	  		<ul class="lista_rel_5">
+	       	  			<li>
+	       	  				<figure class="lista-rel-img">
+	       	  					<img src="images/" alt="" class="lista-img">
+	       	  					<br>
+	       	  					<h2>Imóvel 1</h2>
+	       	  					<br>
+	       	  					<p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit</p>
+	       	  					<br>
+	       	  					<a href="#" class="btn-veja-mais">Veja Mais</a>
+	       	  				</figure>
+	       	  			</li>
+	       	  		</ul>
+       	  			
+       	  		</div>
+
+                <!-- BANNER INFERIOR -->
+       	  		<div id="banner-inferior">
+       	  			<figure class="banner-inferior">
+       	  				<img src="" alt="banner-inferior">
+       	  			</figure>
+       	  		</div>
+       	  		
+       	  	</article>
+       	  </section>
+
+       	  <!-- RODAPÉ -->
+       	  <footer id="rodapé">
+       	  	 <div id="rodapé">
+       	  	 	
+       	  	 </div>
+       	  </footer>
+
 
        </div>
 	</body>
